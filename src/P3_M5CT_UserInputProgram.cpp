95fecde7f4e960e487d11c3d0e355c21b48072aa
Timothy Bjorklund <timothybjorklund@outlook.com>
2026-02-13 23:59:11 -0700

    Initial Commit
    --Project Created
    --Git repository Created
    --GitHub repository Created
    --txt file downloaded and placed inside project folder

---
+++ src/P3_M5CT_UserInputProgram.cpp
@@ -0,0 +1,12 @@
+//============================================================================
+// Name        : P3_M5CT_UserInputProgram.cpp
+// Author      : Timothy Bjorklund
+//============================================================================
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	cout << "Initial Commit and project creation" << endl;
+	return 0;
+}
